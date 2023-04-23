9ef3a77438ef034738c21db9d33225f55f82bcb7
Davi Alves <davialvesb@proton.me>
2023-04-23 19:23:03 +0200

    added card calltoaction

---
+++ src/common/CreateOfferCard.tsx
@@ -0,0 +1,28 @@
+const CreateOfferCard = () => {
+  return (
+    <>
+      <div className='card w-96 bg-base-100 shadow-xl mt-10'>
+        <figure>
+          <img
+            src='https://firebasestorage.googleapis.com/v0/b/alemanha-para-brasileiros.appspot.com/o/app%2Fbrigadeiros.jpeg?alt=media&token=716aa975-6466-4eee-b7aa-736da5b9b4eb'
+            alt='Brigadeiros'
+          />
+        </figure>
+        <div className='card-body'>
+          <h2 className='card-title'>Oferece algum serviço ou produto?</h2>
+          <p>
+            Anuncie gratuitamente para a comunidade de brasileiros na Alemanha.
+            Pague somente se quiser destaque ouro .
+          </p>
+          <div className='card-actions mt-5 '>
+            <button className='btn btn-primary text-emerald-50'>
+              Divulgue grátis
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreateOfferCard;
--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -4,8 +4,8 @@ import AlleBody from '../../common/alle-ui/AlleBody';
 import AlleHeader from '../../common/alle-ui/AlleHeader';
 import CreateOffer from './CreateOffer';
 import { AlleUser } from '../../common/models/AlleUser';
-import { useNavigate } from 'react-router-dom';
-import Carousel from '../../common/ImageCarousel';
+import { Link, useNavigate } from 'react-router-dom';
+import CreateOfferCard from '../../common/CreateOfferCard';
 
 const UserPage = () => {
   const { user }: { user: AlleUser } = useContext(AppContext);
@@ -19,17 +19,26 @@ const UserPage = () => {
     }
   }, []);
 
+  if (!user) {
+    return <></>;
+  }
+
   return (
     <>
       <AlleHeader />
       <AlleBody loading={loading} className='flex-col px-5 min-h-screen'>
-        <h2 className='text-2xl text-slate-600 my-3'>Olá {user.displayName}</h2>
-        <Carousel />
-        <p className='mt-5 text-slate-500 text-sm'>
+        <h2 className='text-2xl text-slate-600 mt-5'>Olá {user.displayName}</h2>
+        <p className='mt-2 text-slate-500 text-sm'>
           Estamos felizes em tê-lo(a) aqui e esperamos que encontre tudo o que
           precisa. Obrigado por se juntar a nós!
         </p>
-        <CreateOffer />
+        <p className='text-slate-500 text-sm'>
+          Você reside em {user.city},{' '}
+          <Link to='/' className='text-emerald-500 underline'>
+            veja as ofertas disponíveis na sua cidade.
+          </Link>
+        </p>
+        <CreateOfferCard />
       </AlleBody>
     </>
   );
